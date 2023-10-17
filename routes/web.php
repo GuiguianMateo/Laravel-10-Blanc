1d83878f369e949962854880ec422e8190def056
Mateo Guiguian <mateoggn04@gmail.com>
2023-10-17 11:51:05 +0200

    commit 11h50

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('/', Controller::class);
 
-Route::resource('menu', MenuController::class);
-Route::resource('sousmenu', SousMenuController::class);
-Route::resource('page', PageController::class);
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('menu', MenuController::class);
+    Route::resource('sousmenu', SousMenuController::class);
+    Route::resource('page', PageController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

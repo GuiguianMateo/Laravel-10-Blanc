18278a56fd03b97fcf799c3b23f4ea1ad99b7b80
Mateo Guiguian <mateoggn04@gmail.com>
2023-12-06 19:14:37 +0100

    bug test unit

--- tests/Feature/TestMenu.php
+++ tests/Feature/TestMenu.php
@@ -7,9 +7,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class TestAdmin extends TestCase
+class TestMenu extends TestCase
 {
-/*     use RefreshDatabase, ActAsUser;*/
+    use RefreshDatabase;
 
     public function test_admin_can_create_menu()
     {
